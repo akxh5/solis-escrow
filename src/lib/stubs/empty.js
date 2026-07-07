6da59d83e01471a0d0b1a4ee5783eacfb4b10fe4
akxh5 <aksh11ansh@gmail.com>
2026-07-07 19:10:30 +0530

    build: add Node.js built-in polyfill stubs for stellar-sdk browser compatibility

---
+++ src/lib/stubs/empty.js
@@ -0,0 +1,4 @@
+// Empty stub for Node.js built-in modules that don't exist in the browser.
+// stellar-sdk / stellar-wallets-kit imports some of these statically
+// but only calls them in Node.js-specific code paths.
+module.exports = {};
